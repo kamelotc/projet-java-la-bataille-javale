12468103bb78a8bf84c089f3af2f5e7769e540c2
kamelotc <camelot.colomban2@gmail.com>
2026-03-06 16:44:59 +0100

    Initialisation du projet Bataille Navale

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module school.coda.lucas.colomban.fxml.projetjavalabataillejavale {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens school.coda.lucas.colomban.fxml.projetjavalabataillejavale to javafx.fxml;
+    exports school.coda.lucas.colomban.fxml.projetjavalabataillejavale;
+}
